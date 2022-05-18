aedb8628a8b376621fa65979d89ee86c8c070906
erfanghorbanee <erfanghorbanee@gmail.com>
2022-05-18 22:58:30 +0430

    Settings updated.

--- simple_api/config/settings.py
+++ simple_api/config/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+}
