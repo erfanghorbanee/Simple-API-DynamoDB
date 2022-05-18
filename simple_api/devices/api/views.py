a28fec68d975fba890d7cd6a615c7a08bf42e098
erfanghorbanee <erfanghorbanee@gmail.com>
2022-05-18 11:43:36 +0430

    DeviceCreateAPI updated and some minors fix.

--- simple_api/devices/api/views.py
+++ simple_api/devices/api/views.py
@@ -1,68 +1,25 @@
 from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from devices.models import Device, DeviceModel
 
-from .serializers import DeviceSerializer
+from .serializers import DeviceCreateSerializer, DeviceDetailSerializer
 
 
-class DeviceCreateAPI(APIView):
+class DeviceCreateAPI(CreateAPIView):
     """
-    A simple api which allows clients to create new instance of Device model
-    or update the one that already exists.
+    A simple api which allows clients to create new instance of Device model.
     """
 
     # permission_classes = (IsAuthenticated,)
 
-    def post(self, request):
-        serializer = DeviceSerializer(data=request.data)
+    queryset = Device.objects.all()
+    serializer_class = DeviceCreateSerializer
 
-        if serializer.is_valid():
-            data = serializer.validated_data
-            name = data.get("name")
-            note = data.get("note")
-            serial = data.get("serial")
-            id = data.get("id").replace("/devices/id", "")
-            deviceModel_id = data.get("deviceModel").replace("/devicemodels/id", "")
-
-            if not id.isdigit():
-                return Response(
-                    status=status.HTTP_400_BAD_REQUEST,
-                    data={"error": "id field must be in this format: /devices/id1"},
-                )
-
-            elif not deviceModel_id.isdigit():
-                return Response(
-                    status=status.HTTP_400_BAD_REQUEST,
-                    data={
-                        "error": "deviceModel field must be in this format: /devicemodels/id1"
-                    },
-                )
-
-            elif (
-                not DeviceModel.objects.only("id")
-                .filter(id=int(deviceModel_id))
-                .exists()
-            ):
-                return Response(
-                    status=status.HTTP_400_BAD_REQUEST,
-                    data={"error": "Device model with this id does not exist."},
-                )
-
-            else:
-                new_device = Device(
-                    id=int(id),
-                    deviceModel_id=int(deviceModel_id),
-                    name=name,
-                    note=note,
-                    serial=serial,
-                )
-                new_device.save()
-
-                return Response(
-                    status=status.HTTP_201_CREATED,
-                )
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(status=status.HTTP_201_CREATED, headers=headers)
