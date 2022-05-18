90e27409fcef5022a21beda57db7fcfc473798b2
erfanghorbanee <erfanghorbanee@gmail.com>
2022-05-19 00:11:58 +0430

    Devices api tests added.

--- simple_api/config/settings.py
+++ simple_api/config/settings.py
@@ -130,5 +130,5 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+    "TEST_REQUEST_DEFAULT_FORMAT": "json",
 }
